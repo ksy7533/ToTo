83f28ddf1608dc21cbe4b6ffb5e552f03e77c083
ksy7533 <ksy7533@gmail.com>
2019-04-10 21:25:44 +0900

    정적파일 경로수정

--- back-end/app.js
+++ back-end/app.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
